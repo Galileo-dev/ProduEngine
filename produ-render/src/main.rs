e0944573f6462f4fd60fd76930d949739116f2be
Galileo <Galileo-dev@users.noreply.github.com>
2021-07-16 10:32:01 +0100

    color triangle

--- produ-render/src/main.rs
+++ produ-render/src/main.rs
@@ -22,7 +22,7 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
 fn main() {
     //?================================================ Config Viewport ================================================
-    let viewport_origin: [f32; 2] = [0.0, 0.0];
+    let viewport_origin: [f32; 2] = [50.0, 0.0];
 
     //?================================================= Main Function =================================================
     //  This gives us the Required Extensions to Start a window"
@@ -72,6 +72,7 @@ fn main() {
     println!("queue: {:?}", queue);
 
     //? Time for a swapchain 😭
+    //? Mine The Bitcoins
     let (mut swapchain, images) = {
         let capabilities = surface.capabilities(physical_device).unwrap();
         let alpha = capabilities
@@ -144,11 +145,20 @@ fn main() {
                 #version 450
                 
                 layout(location = 0) in vec2 position;
-                layout(location = 1) in vec4 vertex;
+
+                layout(location = 1) out vec4 vertex_color;
                 
+                vec4 colors[3] = vec4[3](
+                    vec4(1.0, 0.0, 0.0, 1.0),
+                    vec4(0.0, 1.0, 0.0, 1.0),
+                    vec4(0.0, 0.0, 1.0, 1.0)
+                );
+
                 void main() {
                     gl_Position = vec4(position,0.0, 1.0);
-                }"
+                    vertex_color = colors[gl_VertexIndex];
+                }
+                "
         }
     }
 
@@ -156,14 +166,15 @@ fn main() {
         vulkano_shaders::shader! {
             ty: "fragment",
             src: "
-                    #version 450
+                #version 450
+                layout(location = 0) out vec4 f_color;
+                
+                layout(location = 1) in vec4 vertex_color;
                     
-                    layout(location = 0) out vec4 f_color;
-                    in vec4 vertex_color;
-                    
-                    void main() {
-                        f_color = vec4(103.0, 58.0, 183.0, 1.0);
-                    }"
+                void main() {
+                    f_color = vertex_color;
+                }
+                "
         }
     }
 
